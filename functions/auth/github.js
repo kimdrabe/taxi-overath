ef1d151f96323a0ddd11f29bbfa12ab1f1f34e40
Taxi Overath <info@taxi-overath.de>
2026-05-12 20:59:18 +0200

    CMS: direkter auth/github endpoint ohne init-page

--- functions/auth/github.js
+++ functions/auth/github.js
@@ -1,9 +1,21 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const code = url.searchParams.get('code');
+  const siteId = url.searchParams.get('site_id') || 'taxi-overath.pages.dev';
+
+  const clientId = 'Ov23liNbp3aoeUEvfF3f';
+  const clientSecret = '8d70c4d8bb6be5d291223526bc6953e9e6be5e82';
 
   if (!code) {
-    return new Response('No code provided', { status: 400 });
+    const redirectUri = 'https://' + siteId + '/auth/github';
+    const githubUrl = 'https://github.com/login/oauth/authorize'
+      + '?client_id=' + encodeURIComponent(clientId)
+      + '&redirect_uri=' + encodeURIComponent(redirectUri)
+      + '&scope=repo'
+      + '&response_type=code'
+      + '&state=' + encodeURIComponent(siteId);
+
+    return Response.redirect(githubUrl, 302);
   }
 
   const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
@@ -13,8 +25,8 @@ export async function onRequest(context) {
       'Accept': 'application/json',
     },
     body: JSON.stringify({
-      client_id: 'Ov23liNbp3aoeUEvfF3f',
-      client_secret: '8d70c4d8bb6be5d291223526bc6953e9e6be5e82',
+      client_id: clientId,
+      client_secret: clientSecret,
       code: code,
     }),
   });
@@ -29,22 +41,15 @@ export async function onRequest(context) {
     });
   }
 
-  const html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <title>Authorizing...</title>
-</head>
-<body>
-  <script>
-    var token = ${JSON.stringify(accessToken)};
-    window.opener.postMessage({ type: 'authorization', token: token }, '*');
-    window.close();
-  </script>
-  <p>Authorizing... fertig!</p>
-</body>
-</html>`;
+  const html = '<!DOCTYPE html><html><head><meta charset="utf-8"/><title>Authorizing...</title></head><body><script>' +
+    'var token = ' + JSON.stringify(accessToken) + ';' +
+    'if (window.opener) {' +
+    '  window.opener.postMessage({ type: "authorization", token: token }, "*");' +
+    '  window.close();' +
+    '} else {' +
+    '  document.body.textContent = "Authorizing... done!";' +
+    '}' +
+    '<\/script><p>Authorizing... fertig!</p></body></html>';
 
   return new Response(html, {
     headers: { 'Content-Type': 'text/html' },
