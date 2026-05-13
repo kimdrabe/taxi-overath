98a1c62286d78978a1bf736cd68cfe29ea10f7ba
kimdrabe <kimdrabe@users.noreply.github.com>
2026-05-13 14:34:24 +0200

    Add GitHub Pages deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kimdrabe.github.io',
+  base: '/taxi-overath',
+
   vite: {
     plugins: [tailwindcss()]
   },
